179633ebd2e1a647381224f25d8301fe4a005ec6
张俊凯 <zhangjunkai@qianxin.com>
2024-03-26 11:35:39 +0800

    feat: 首页开发

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,48 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import styles from './index.module.scss';
+import Link from 'next/link';
 
 function Page() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: 12,
-      }}
-    >
-      <ConnectButton />
+    <div className={styles.index}>
+      <div className={styles.container}>
+        <h1>Web3 Integrate</h1>
+        <ul className={styles.main}>
+          <li>
+            <Link href="/wallet-connect">
+              <h2>钱包连接</h2>
+              <p>使用 ethers.js、RainbowKit 连接 Metamask，实现了连接、断开、查询余额、刷新余额、转账等加密钱包的极简功能。</p>
+            </Link>
+          </li>
+          <li>
+            <h2>钱包连接</h2>
+            <p>使用 ethers.js、RainbowKit 连接 Metamask，实现了连接、断开、查询余额、刷新余额、转账等加密钱包的极简功能。</p>
+          </li>
+          <li>
+            <h2>钱包连接</h2>
+            <p>使用 ethers.js、RainbowKit 连接 Metamask，实现了连接、断开、查询余额、刷新余额、转账等加密钱包的极简功能。</p>
+          </li>
+          <li>
+            <h2>钱包连接</h2>
+            <p>使用 ethers.js、RainbowKit 连接 Metamask，实现了连接、断开、查询余额、刷新余额、转账等加密钱包的极简功能。</p>
+          </li>
+          <li>
+            <h2>钱包连接</h2>
+            <p>使用 ethers.js、RainbowKit 连接 Metamask，实现了连接、断开、查询余额、刷新余额、转账等加密钱包的极简功能。</p>
+          </li>
+          <li>
+            <h2>钱包连接</h2>
+            <p>使用 ethers.js、RainbowKit 连接 Metamask，实现了连接、断开、查询余额、刷新余额、转账等加密钱包的极简功能。</p>
+          </li>
+          <li>
+            <h2>钱包连接</h2>
+            <p>使用 ethers.js、RainbowKit 连接 Metamask，实现了连接、断开、查询余额、刷新余额、转账等加密钱包的极简功能。</p>
+          </li>
+          <li>
+            <h2>钱包连接</h2>
+            <p>使用 ethers.js、RainbowKit 连接 Metamask，实现了连接、断开、查询余额、刷新余额、转账等加密钱包的极简功能。</p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
